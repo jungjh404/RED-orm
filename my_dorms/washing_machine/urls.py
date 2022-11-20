5b3fde1603e2e8a7be0bc76157396cfb69025a16
jungjh404 <jungjh404@gmail.com>
2022-11-20 17:08:31 +0900

    FEAT: Reserve

--- my_dorms/washing_machine/urls.py
+++ my_dorms/washing_machine/urls.py
@@ -6,5 +6,7 @@ app_name = 'washing_machine'
 urlpatterns = [
     path('', views.status, name='status'),
     path('camera/', views.camera, name='camera'),
+    path('reserve_camera/', views.reserve_camera, name='reserve_camera'),
     path('add/', views.add, name='add'),
+    path('reserve/', views.reserve, name='reserve'),
 ]
--- my_dorms/washing_machine/webpush.py
+++ my_dorms/washing_machine/webpush.py
@@ -6,12 +6,15 @@ from webpush import send_group_notification
 
 import json # https://github.com/safwanrahman/django-webpush/issues/71
 
-def machine_done_webpush(state):
+def machine_done_webpush(state, reserve_lst):
     # User = get_user_model()
     # users = User.objects.all()
 
     users = []
     users.append(state.user_id)
+
+    for reserve in reserve_lst:
+        users.append(reserve.user_id)
 
     payload = {"head": f"세탁완료", "body": f"{state.machine_id}의 세탁이 완료되었습니다."}
 
